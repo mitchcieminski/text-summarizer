55d26b49af5bfddcaa242105d0723d691d7554ed
Abe Levitan <allevitan@gmail.com>
2013-04-23 10:08:28 -0400

    Add method for creating markov matrix from file

--- markov.py
+++ markov.py
@@ -1,5 +1,26 @@
 import numpy as np
 from scipy.sparse import coo_matrix, csr_matrix
+import re
 
-print "Hi, Everybody!"
-print "Hi, Dr. Nick!"
+def build_markov(textfile):
+
+    r,c,v = [],[],[]# The building blocks of the matrix
+    wordloc = {}
+    numwords = 0
+
+    for line in textfile.xreadlines():
+        line = re.findall(word_re, line.lower())
+        for words in zip(line[:-1], line[1:]):
+            indices = [wordloc.get(words[0], -1),wordloc.get(words[1], -1)]
+            for i, (ind,w) in enumerate(zip(indices, words)):
+                if ind == -1:
+                    wordloc[w] = numwords
+                    numwords += 1
+                    indices[i] = wordloc[w]
+            c.append(indices[0])
+            r.append(indices[1])
+            v.append(1)
+    markmat = coo_matrix((v,(r,c)), shape=(numwords, numwords))
+    
+    return (wordloc, markmat)
+
