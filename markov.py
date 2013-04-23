8edb4233dea80e552609cd1a73c36f9c300f3051
Abe Levitan <allevitan@gmail.com>
2013-04-23 10:16:09 -0400

    Add skeleton method to make 2 matrices comparable

--- markov.py
+++ markov.py
@@ -21,6 +21,12 @@ def build_markov(textfile):
             r.append(indices[1])
             v.append(1)
     markmat = coo_matrix((v,(r,c)), shape=(numwords, numwords))
-    
+
     return (wordloc, markmat)
 
+def compare_matrices( tuple0, tuple1 ):
+    markmat, wordloc = (tuple0[0], tuple1[0]), (tuple0[1], tuple1[1])
+    
+    # Do stuff to markmat and wordloc to make the matrices the same size
+    
+    return ((markmat[0], wordloc[0]), (matkmat[1], wordloc[1]))
