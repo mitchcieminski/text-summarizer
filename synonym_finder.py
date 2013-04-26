1067bd23b08e3127d914b3a87bbe7bcdeca15120
Abraham Levitan <allevitan@gmail.com>
2013-04-25 23:43:05 -0400

    Worked on synonym_finder

--- synonym_finder.py
+++ synonym_finder.py
@@ -1,9 +1,24 @@
-import markov
+import markov as m
+import numpy as np
 
-(markov, wordloc) = markov.build_markov('testdata/1342.txt')
+(markov, wordloc) = m.build_markov(open('testdata/1342.txt'))
 
-thing = normalize(markov) * normalize(markov.transpose())
+print 'Built'
 
-thing2 = thing ** 100
+thing = m.normalize(markov) * m.normalize(markov.transpose())
 
-print thing2
+print 'normalized and multiplied'
+
+revloc = {}
+for word, index in wordloc.iteritems():
+    revloc[index] = word
+words = [revloc[i] for i in range(0,thing.shape[0])]
+pairs = []
+
+for i in range(0,thing.shape[0]):
+    problist = thing.getcol(i).todense().transpose().tolist()[0]
+    coupled = zip(*sorted(zip(problist, words)))
+    pairs.append((words[i], coupled[1][-4:]))
+
+for pair in pairs[100:110]:
+    print pair
